f3783ea6b27fef366a6782f8f43744707294f089
Matheus Rosmaninho <matheusrosmano@yahoo.com>
2023-04-09 16:34:12 -0300

    ci: Ajuste the cidr blocks

--- terraform/infra/vpc.tf
+++ terraform/infra/vpc.tf
@@ -5,8 +5,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs = ["${var.region}a"]
-  private_subnets = [ "10.0.0.1/24" ]
-  public_subnets = [ "10.0.0.101/24" ]
+  private_subnets = [ "10.0.1.0/24" ]
+  public_subnets = [ "10.0.101.0/24" ]
 
   enable_dns_hostnames = true
   enable_dns_support = true
